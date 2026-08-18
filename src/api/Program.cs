eb5921c119f632ef0d4ed1fb3e10123bc0494d9a
Deven Poland-Starn <deven@dpsbuilds.dev>
2026-08-18 15:14:54 -0500

    infra: Updated API Deploy Workflow and Added Task Definition

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -6,6 +6,7 @@ using LinqToDB.AspNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHealthChecks();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
@@ -45,29 +46,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("AllowAngular");
-
 app.MapControllers();
-
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapHealthChecks("/health");
 
 app.Run();
 
